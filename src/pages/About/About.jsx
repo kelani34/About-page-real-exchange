05693f09069fe71fc1f378f394b6776e9ff5ecd4
kelani34 <kelanitaiwo34@gmail.com>
2022-11-18 13:39:39 +0100

    Finish up

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -3,7 +3,15 @@ import styled from "styled-components";
 import mobileImage from "./assets/mobile-image.png";
 import missionImage from "./assets/mission-image.png";
 import visionImage from "./assets/vision-image.png";
-
+import Value from "./components/Value";
+import {
+  AppstoreIcon,
+  EntrepreneurshipIcon,
+  GoogleplayIcon,
+} from "./assets/icons/icons";
+import TeamMember from "./components/TeamMember";
+import teamate1 from "./assets/person1.png";
+import scan from "./assets/scan-image.png";
 const About = () => {
   return (
     <>
@@ -47,14 +55,127 @@ const About = () => {
         </MissionVisionSection>
 
         <ValueSection>
-          <ValueContent>
-            <ValueHead>Our Values</ValueHead>
-            <ValueText>
+          <StyledContent>
+            <StyledHead>Our Values</StyledHead>
+            <StyledText>
               Offering a unique experience and providing an accurate of any
-              country exchange rate of your choice{" "}
-            </ValueText>
-          </ValueContent>
+              country exchange rate of your choice
+            </StyledText>
+          </StyledContent>
+          <ValuesList>
+            <Value
+              icon={<EntrepreneurshipIcon />}
+              text="Ability to identify problems, and collaboratively solve them to create opportunities for everyone."
+              heading="Entrepreneurship"
+            />
+            <Value
+              icon={<EntrepreneurshipIcon />}
+              text="Ability to identify problems, and collaboratively solve them to create opportunities for everyone."
+              heading="Entrepreneurship"
+            />
+            <Value
+              icon={<EntrepreneurshipIcon />}
+              text="Ability to identify problems, and collaboratively solve them to create opportunities for everyone."
+              heading="Entrepreneurship"
+            />
+            <Value
+              icon={<EntrepreneurshipIcon />}
+              text="Ability to identify problems, and collaboratively solve them to create opportunities for everyone."
+              heading="Entrepreneurship"
+            />
+          </ValuesList>
         </ValueSection>
+
+        <TeamSection>
+          <StyledContent>
+            <StyledHead>Meet the Brilliant Team</StyledHead>
+            <StyledText>
+              Meet our team of digital warriors ready to harness the force for
+              good
+            </StyledText>
+          </StyledContent>
+          <TeamMembers>
+            <TeamMember
+              image={teamate1}
+              name="Person Name"
+              designation="designation"
+            />
+            <TeamMember
+              image={teamate1}
+              name="Person Name"
+              designation="designation"
+            />
+            <TeamMember
+              image={teamate1}
+              name="Person Name"
+              designation="designation"
+            />
+            <TeamMember
+              image={teamate1}
+              name="Person Name"
+              designation="designation"
+            />
+            <TeamMember
+              image={teamate1}
+              name="Person Name"
+              designation="designation"
+            />
+            <TeamMember
+              image={teamate1}
+              name="Person Name"
+              designation="designation"
+            />
+            <TeamMember
+              image={teamate1}
+              name="Person Name"
+              designation="designation"
+            />
+            <TeamMember
+              image={teamate1}
+              name="Person Name"
+              designation="designation"
+            />
+            <TeamMember
+              image={teamate1}
+              name="Person Name"
+              designation="designation"
+            />
+            <TeamMember
+              image={teamate1}
+              name="Person Name"
+              designation="designation"
+            />
+            <TeamMember
+              image={teamate1}
+              name="Person Name"
+              designation="designation"
+            />
+          </TeamMembers>
+        </TeamSection>
+
+        <DownloadSection>
+          <Img src={mobileImage} />
+          <DownloadView>
+            <DownloadContent>
+              <DownloadHeader>Download Streetrates App</DownloadHeader>
+              <DownloadText>
+                Check live rates, send money securely, set rate alerts, receive
+                notifications and more.
+              </DownloadText>
+              <Scan>
+                <img src={scan} />
+              </Scan>
+              <DownloadIcons>
+                <AppleDownload>
+                  <AppstoreIcon />
+                </AppleDownload>
+                <GoogleDownload>
+                  <GoogleplayIcon />
+                </GoogleDownload>
+              </DownloadIcons>
+            </DownloadContent>
+          </DownloadView>
+        </DownloadSection>
       </StyledPage>
     </>
   );
@@ -65,12 +186,14 @@ export default About;
 const sizes = {
   mobile: "490px",
   tablet: "650px",
-  laptop: "1280px",
+  laptopS: "1024px",
+  laptop: "1184px",
 };
 
 const devices = {
   mobile: `(max-width: ${sizes.mobile})`,
   tablet: `(max-width: ${sizes.tablet})`,
+  laptopS: `(max-width: ${sizes.laptop})`,
   laptop: `(max-width: ${sizes.laptop})`,
 };
 const StyledPage = styled.div``;
@@ -82,8 +205,13 @@ const AboutSection = styled.div`
   padding: 4rem 177px;
 
   @media ${devices.laptop} {
+    flex-direction: row;
+    padding: 42px 65px;
+    align-items: center;
+  }
+  @media ${devices.laptopS} {
     flex-direction: column;
-    padding: 42px 224px;
+    padding: 42px 65px;
     align-items: center;
   }
   @media ${devices.tablet} {
@@ -108,7 +236,7 @@ const AboutContent = styled.div`
   font-family: var(--font-family);
   /* max-width: 490px; */
 
-  @media ${devices.laptop} {
+  @media ${devices.laptopS} {
     margin: 0;
     display: flex;
     align-items: center;
@@ -136,7 +264,7 @@ const AboutImage = styled.div`
   font-family: var(--font-family);
 `;
 const AboutHead = styled.h1`
-  margin: 1.5rem 0;
+  margin: 50px 0;
   font-size: 36px;
   font-weight: 600;
   line-height: 44px;
@@ -146,15 +274,13 @@ const AboutText = styled.p`
   font-weight: 600;
   line-height: 28px;
 
-  @media ${devices.mobile} {
-    /* font-size: 16px; */
+  @media ${devices.laptop} {
+    font-size: 16px;
   }
 `;
 
 const Img = styled.img`
-  @media ${devices.laptop} {
-    width: 144px;
-  }
+  width: 422px;
   @media ${devices.tablet} {
     width: 105px;
   }
@@ -237,16 +363,142 @@ const ValueSection = styled.div`
   color: #202020;
   font-family: var(--font-family);
   padding: 100px 177px;
+
+  @media ${devices.laptopS} {
+    flex-direction: column;
+    padding: 42px 65px;
+    align-items: center;
+  }
+  @media ${devices.tablet} {
+    flex-direction: column;
+    padding: 42px 44px;
+    align-items: center;
+  }
+  @media ${devices.mobile} {
+    /* flex-direction: column; */
+    padding: 42px 44px;
+    /* align-items: center; */
+  }
 `;
-const ValueContent = styled.div`
+const StyledContent = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  margin-bottom: 44px;
 `;
-const ValueHead = styled.h2`
+const StyledHead = styled.h2`
   font-weight: 600;
   font-size: 36px;
   line-height: 44px;
   margin-bottom: 28px;
 `;
-const ValueText = styled.p``;
+const StyledText = styled.p``;
+
+const ValuesList = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  gap: 10px;
+  @media ${devices.laptop} {
+    justify-content: center;
+    width: 100%;
+    gap: 15px;
+    max-width: 850px;
+  }
+  @media ${devices.tablet} {
+    flex-direction: column;
+    width: auto;
+  }
+`;
+
+const TeamSection = styled.div`
+  background-color: #fff;
+  display: flex;
+  flex-direction: column;
+  color: #202020;
+  font-family: var(--font-family);
+  padding: 100px 177px;
+
+  @media ${devices.laptop} {
+    flex-direction: column;
+    padding: 42px 224px;
+    align-items: center;
+    width: 730px;
+  }
+  @media ${devices.tablet} {
+    flex-direction: column;
+    padding: 42px 44px;
+    align-items: center;
+    width: auto;
+  }
+  @media ${devices.mobile} {
+    /* flex-direction: column; */
+    padding: 42px 44px;
+    /* align-items: center; */
+    width: auto;
+  }
+`;
+
+const TeamContent = styled.div``;
+
+const TeamMembers = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 44px;
+  justify-content: center;
+`;
+
+const DownloadSection = styled.div`
+  background-color: #fff;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  /* justify-content: space-between; */
+  color: #202020;
+  font-family: var(--font-family);
+  padding: 100px 177px;
+
+  @media ${devices.laptop} {
+    flex-direction: column;
+    padding: 42px 224px;
+    align-items: center;
+  }
+  @media ${devices.tablet} {
+    flex-direction: column;
+    padding: 42px 44px;
+    align-items: center;
+  }
+  @media ${devices.mobile} {
+    /* flex-direction: column; */
+    padding: 42px 44px;
+    /* align-items: center; */
+  }
+`;
+const DownloadView = styled.div``;
+const DownloadContent = styled.div`
+  margin: 0 158px;
+`;
+const DownloadHeader = styled.div`
+  font-weight: 600;
+  font-size: 22px;
+  line-height: 28px;
+`;
+const DownloadText = styled.div`
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 27px;
+  margin-top: 24px;
+  margin-bottom: 34px;
+`;
+const Scan = styled.div`
+  margin-bottom: 24px;
+`;
+
+const GoogleDownload = styled.a``;
+
+const AppleDownload = styled.a``;
+const DownloadIcons = styled.div`
+  display: flex;
+  gap: 10px;
+`;
---
+++ src/pages/About/components/TeamMember.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import styled from "styled-components";
+import teamMember from "../assets/person1.png";
+const TeamMember = (props) => {
+  return (
+    <>
+      <StyledTeam>
+        <img src={props.image} />
+        <h2>{props.name}</h2>
+        <span>{props.designation}</span>
+      </StyledTeam>
+    </>
+  );
+};
+
+export default TeamMember;
+
+const StyledTeam = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-width: 225px;
+  align-items: center;
+  gap: 15px;
+`;
